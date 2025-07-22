6f96f55a670a15eece75fcf396feb7af566e7820
Sean Persad <seanpdev211@gmail.com>
2025-07-22 10:40:29 +0900

    implemented exporting csv

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -11,7 +11,7 @@ import {
     isValid
 } from 'date-fns';
 import { ja } from 'date-fns/locale';
-import { FeatureItem, FeatureParams, ImageDetail } from "./types";
+import { FeatureItem, FeatureParams, ImageDetail, JobDetail } from "./types";
 import { UPLOADS_BASE_URL } from "./config";
 import { MapData, PrefectureOptions } from "./constants";
 import { Area } from "react-easy-crop";
@@ -229,4 +229,25 @@ export const getFilterJobUrl = (value: JobFilterFormValue, featureList: FeatureI
     const cString = !conditions?.length ? 'na' : featureList.filter((i: FeatureItem) => conditions.includes(i.id)).map((i: FeatureItem) => i.name).join('-');
     const eString = !employmentTypes?.length ? 'na' : featureList.filter((i: FeatureItem) => employmentTypes.includes(i.id)).map((i: FeatureItem) => i.name).join('-');
     return `/job-openings/${pString}/${jString}/${iString}/${cString}/${eString}`
+}
+
+export const generateCSVData = (data: JobDetail[]) => {
+    return data.map(item => ({
+        'ID': item.id,
+        'Job Title': item.job_title,
+        'Job Posting Date': item.job_posting_date,
+        'Job Category': item.job_category,
+        'Job Introduction': item.job_lead_statement,
+        'Salary': item.pay,
+        'Public Status': item.public_status,
+        'Job Type': item.job_detail_page_template_id === 1 ? '直接応募のみ' : '転職⽀援サービス',
+        'Thumbnail': getFirstFullImage(item.jobThumbnails),
+        'Employment Type': item.employmentType.name,
+        'Employer': item.employer.clinic_name,
+        'Public Start Date': item.clinic_public_date_start,
+        'Public End Date': item.clinic_public_date_end,
+        'Youtube Url': item.youtube_url,
+        'Another Url': item.another_url_text,
+        'Features': item.features.map(i => i.name).join(',')
+    }));
 }
