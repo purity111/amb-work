8a824294fc93c4f449a3d692684e9721b6e1d6a2
Sean Persad <seanpdev211@gmail.com>
2025-07-22 21:01:39 +0900

    fixed bug-63

--- src/components/Cropper.tsx
+++ src/components/Cropper.tsx
@@ -33,7 +33,7 @@ export default function CustomCropper({ image, cropWidth, cropHeight, onCancel, 
 
     return (
         <div className="fixed inset-0 z-1000 w-[100vw] h-[100vh] flex flex-row justify-center items-center bg-black/60">
-            <div className="w-[90%] max-w-[1000px] h-[80%] p-20 bg-white rounded-sm overflow-hidden flex flex-col">
+            <div className="w-[90%] max-w-[1000px] h-[80%] p-4 sm:p-20 bg-white rounded-sm overflow-hidden flex flex-col">
                 <div className="relative flex-1">
                     <Cropper
                         image={image}
@@ -41,11 +41,33 @@ export default function CustomCropper({ image, cropWidth, cropHeight, onCancel, 
                         zoom={zoom}
                         aspect={1} // 1:1 for square crop
                         onCropChange={setCrop}
-                        onZoomChange={setZoom}
+                        zoomWithScroll={false}
                         minZoom={0.2}
                         cropShape="rect"
                         cropSize={{ width: cropWidth, height: cropHeight }}
                         onCropComplete={onCropComplete}
+                    />
+                </div>
+                <div className="w-full mx-auto py-4">
+                    <label htmlFor="slider" className="block mb-2 text-sm font-medium text-gray-700">
+                        Zoom: {zoom}
+                    </label>
+                    <input
+                        id="slider"
+                        type="range"
+                        min={0.2}
+                        max={3}
+                        value={zoom}
+                        step={0.2}
+                        onChange={(e) => setZoom(Number(e.target.value))}
+                        className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer range-sm
+                   [&::-webkit-slider-thumb]:appearance-none
+                   [&::-webkit-slider-thumb]:h-4
+                   [&::-webkit-slider-thumb]:w-4
+                   [&::-webkit-slider-thumb]:rounded-full
+                   [&::-webkit-slider-thumb]:bg-blue-600
+                   [&::-webkit-slider-thumb]:shadow-md
+                   [&::-webkit-slider-thumb]:hover:bg-blue-700"
                     />
                 </div>
                 <div className="flex flex-row pt-10 justify-between">
