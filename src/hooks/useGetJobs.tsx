68d7d284ec8714f73f09fe98bf5f0ee09c454499
strong <strong.business.info@gmail.com>
2025-08-19 11:22:47 +0900

    recommend-fix 2

--- src/hooks/useGetJobs.tsx
+++ src/hooks/useGetJobs.tsx
@@ -3,10 +3,10 @@ import { JobFetchParam } from '@/utils/types'
 import { useQuery } from '@tanstack/react-query'
 
 const useGetJobs = (param: JobFetchParam) => {
-    const { page, limit, searchTerm, jobType, isAdmin, companyID, employer_id, features, prefectures, public_status, sortBy, recommend } = param;
+    const { page, limit, searchTerm, jobType, isAdmin, companyID, employer_id, features, prefectures, public_status, sortBy } = param;
     return useQuery({
-        queryKey: ['getJobs', page, limit, searchTerm, jobType, isAdmin, companyID, employer_id, features, prefectures, public_status, sortBy, recommend],
-        queryFn: () => getJobs(page, limit, searchTerm, jobType, isAdmin, companyID, employer_id, features, prefectures, public_status, sortBy, recommend),
+        queryKey: ['getJobs', page, limit, searchTerm, jobType, isAdmin, companyID, employer_id, features, prefectures, public_status, sortBy],
+        queryFn: () => getJobs(page, limit, searchTerm, jobType, isAdmin, companyID, employer_id, features, prefectures, public_status, sortBy),
         refetchOnMount: false,
         refetchOnWindowFocus: false,
         refetchOnReconnect: false,
