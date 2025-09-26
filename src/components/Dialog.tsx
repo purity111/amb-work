364b1cf98cfd3e284145b93a20ec0f54e5def657
strong <strong.business.info@gmail.com>
2025-09-26 21:00:53 +0900

    apply modal fix

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -27,7 +27,7 @@ export default function Dialog({
                     <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                         <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                             <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                <h3 className="text-[20px] md:text-[24px] font-semibold text-center text-blue" id="modal-title">{title}</h3>
+                                <h3 className={`text-[20px] md:text-[24px] font-semibold text-center ${jobTypeColor === 'orange' ? 'text-orange' : 'text-blue'}`} id="modal-title">{title}</h3>
                                 <div className="my-2">
                                     <p className="text-sm text-gray-500" dangerouslySetInnerHTML={{ __html: description }}></p>
                                 </div>
